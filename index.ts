63bdbe8aa8802139456c7417686a9b04fdd2ad8b
Julia Tufts <juliatufts@gmail.com>
2025-10-22 15:35:59 -0400

    fix things (I think)

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-import './index.css';
-import { addDirectionalLight } from './src/3d/lights.ts';
-import { PerspectiveCamera, Raycaster, Scene, Vector2, WebGLRenderer } from 'three';
-import { desert, land } from './src/desert.ts';
-import { people, addPerson, positionPeople } from './src/people.ts'
-import { disposeOf } from './src/3d/gltf.ts';
-import { loadScene, saveScene } from './src/storage.ts';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import "./index.css";
+import { addDirectionalLight } from "./src/3d/lights.ts";
+import {
+  PerspectiveCamera,
+  Raycaster,
+  Scene,
+  Vector2,
+  WebGLRenderer,
+} from "three";
+import { desert, land } from "./src/desert.ts";
+import { people, addPerson, positionPeople } from "./src/people.ts";
+import { disposeOf } from "./src/3d/gltf.ts";
+import { loadScene, saveScene } from "./src/storage.ts";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-
-
-// Set up 
+// Set up
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -19,7 +23,6 @@ const camera = new PerspectiveCamera(75, WIDTH / HEIGHT);
 camera.position.y = 10;
 camera.position.z = 80;
 
-
 const renderer = new WebGLRenderer();
 renderer.setSize(WIDTH, HEIGHT);
 document.body.appendChild(renderer.domElement);
@@ -28,161 +31,124 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const overlay = document.createElement('div');
-overlay.id = 'overlay';
+const overlay = document.createElement("div");
+overlay.id = "overlay";
 document.body.appendChild(overlay);
 
-
 // Saved "game" state
-const sceneData = {
-    colors: {
-        cacti: '#FFFFFF',
-        desert: '#a5988a',
-    },
-    people: [
-        { position: { x: 0, z: 0 } },
-    ]
-};
-// console.log('sceneData', sceneData);
+const sceneData = loadScene();
 
 //// colors
 
 function getColorPicker(name: string, startColor: string) {
-    const colorPicker = document.createElement('div');
-    colorPicker.classList.add('buttonish');
-    const label = document.createElement('label');
-    label.setAttribute('for', `color-${name}`);
-    label.innerText = name;
-    colorPicker.appendChild(label);
-    const input = document.createElement('input');
-    input.type = 'color';
-    input.name = `color-${name}`;
-    input.id = `color-${name}`;
-    input.value = startColor;
-    colorPicker.appendChild(input);
+  const colorPicker = document.createElement("div");
+  colorPicker.classList.add("buttonish");
+  const label = document.createElement("label");
+  label.setAttribute("for", `color-${name}`);
+  label.innerText = name;
+  colorPicker.appendChild(label);
+  const input = document.createElement("input");
+  input.type = "color";
+  input.name = `color-${name}`;
+  input.id = `color-${name}`;
+  input.value = startColor;
+  colorPicker.appendChild(input);
 
-    return colorPicker;
+  return colorPicker;
 }
 
-const cactiColorPicker = getColorPicker('cacti', sceneData.colors.cacti);
-cactiColorPicker.addEventListener('input', (e) => {
-    const input = e.target as HTMLInputElement;
-    light.color.set(input.value);
-    sceneData.colors.cacti = input.value;
-    saveScene(sceneData);
-});
-overlay.appendChild(cactiColorPicker);
+// const cactiColorPicker = getColorPicker('cacti', sceneData.colors.cacti);
+// cactiColorPicker.addEventListener('input', (e) => {
+//     const input = e.target as HTMLInputElement;
+//     light.color.set(input.value);
+//     sceneData.colors.cacti = input.value;
+//     saveScene(sceneData);
+// });
+// overlay.appendChild(cactiColorPicker);
 
+// const desertColorPicker = getColorPicker('desert', sceneData.colors.desert);
+// desertColorPicker.addEventListener('input', (e) => {
+//     const input = e.target as HTMLInputElement;
+//     land.material.color.set(input.value);
+//     sceneData.colors.desert = input.value;
+//     saveScene(sceneData);
+// });
+// overlay.appendChild(desertColorPicker);
 
-const desertColorPicker = getColorPicker('desert', sceneData.colors.desert);
-desertColorPicker.addEventListener('input', (e) => {
-    const input = e.target as HTMLInputElement;
-    land.material.color.set(input.value);
-    sceneData.colors.desert = input.value;
-    saveScene(sceneData);
-});
-overlay.appendChild(desertColorPicker);
-
-
-//// new cactus
+//// new pumpkin person
 function getButton(name: string, text: string) {
-    const button = document.createElement('button');
-    button.id = `button-${name}`;
-    button.innerText = text;
-    return button;
+  const button = document.createElement("button");
+  button.id = `button-${name}`;
+  button.innerText = text;
+  return button;
 }
 
-function cactusPositioner(person: Scene) {
-    if (!person)
-        throw new Error('cannot position a nonexistent person!');
-    const raycaster = new Raycaster();
-    const pointer = new Vector2();
-    const onPointerMove = (e: PointerEvent) => {
-        pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
-        pointer.y = - (e.clientY / window.innerHeight) * 2 + 1;
-        raycaster.setFromCamera(pointer, camera);
-        const intersection = raycaster.intersectObject(land)[0]?.point;
-        if (intersection) {
-            person.position.x = intersection.x;
-            person.position.z = intersection.z;
-        }
+function personPositioner(person: Scene) {
+  if (!person) throw new Error("cannot position a nonexistent person!");
+  const raycaster = new Raycaster();
+  const pointer = new Vector2();
+  const onPointerMove = (e: PointerEvent) => {
+    pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(pointer, camera);
+    const intersection = raycaster.intersectObject(land)[0]?.point;
+    if (intersection) {
+      person.position.x = intersection.x;
+      person.position.z = intersection.z;
     }
-    return onPointerMove;
-
+  };
+  return onPointerMove;
 }
-const newButton = getButton('new', 'new cactus');
-newButton.addEventListener('click', async (event) => {
-    // const cactusType = ///Math.ceil(Math.random() * 5) as CactusIndex;
-    const newPerson = await addPerson();
-    if (!newPerson) {
-        throw new Error('error adding person');
-    }
 
-    const onPointerMove = cactusPositioner(newPerson);
-    window.addEventListener('pointermove', onPointerMove);
-    onPointerMove(event); // set initial position
+const newButton = getButton("new", "new pumpkin person");
+newButton.addEventListener("click", async (event) => {
+  const newPerson = await addPerson();
+  if (!newPerson) {
+    throw new Error("error adding person");
+  }
 
-    const onClick = () => {
-        window.removeEventListener('pointermove', onPointerMove);
-        sceneData.people.push({
-            position: {
-                x: newPerson.position.x,
-                z: newPerson.position.z
-            }
-        });
-        saveScene(sceneData);
-        window.removeEventListener('click', onClick);
-    }
-    window.addEventListener('click', onClick);
+  const onPointerMove = personPositioner(newPerson);
+  window.addEventListener("pointermove", onPointerMove);
+  onPointerMove(event); // set initial position
 
-})
+  const onClick = () => {
+    window.removeEventListener("pointermove", onPointerMove);
+    sceneData.people.push({
+      position: {
+        x: newPerson.position.x,
+        z: newPerson.position.z,
+      },
+    });
+    saveScene(sceneData);
+    window.removeEventListener("click", onClick);
+  };
+  window.addEventListener("click", onClick);
+});
 overlay.appendChild(newButton);
 
 // Clear scene
-const clearButton = getButton('clear', 'clear cacti');
-clearButton.addEventListener('click', () => {
-    while (people.children.length > 0) {
-        const last = people.children.pop();
-        if (last) {
-            disposeOf(last);
-        }
+const clearButton = getButton("clear", "clear pumpkin people");
+clearButton.addEventListener("click", () => {
+  while (people.children.length > 0) {
+    const last = people.children.pop();
+    if (last) {
+      disposeOf(last);
     }
-    saveScene({ ...sceneData, people: [] });
+  }
+  saveScene({ ...sceneData, people: [] });
 });
 overlay.appendChild(clearButton);
 
-
-
-
 // Pull it all together and set initial colors
-
-
-// wrap cacti in their own scene so lighting changes color
-// const litPeople = new Scene();
-// const stringToColor = (color: string) => parseInt(color.replace('#', '0x'));
-// const light = addDirectionalLight(litPeople, stringToColor(sceneData.colors.cacti), 10);
-
-console.log('sceneData', sceneData);
+addDirectionalLight(scene);
 
 await positionPeople(sceneData.people);
-// litPeople.add(people);
 scene.add(people);
 
 land.material.color.set(sceneData.colors.desert);
 scene.add(desert);
 
-
-
 // Render
-renderer.setAnimationLoop(() =>
-    renderer.render(scene, camera)
-)
+renderer.setAnimationLoop(() => renderer.render(scene, camera));
 
-
-
-
-export {
-    renderer,
-    scene,
-    camera
-}
+export { renderer, scene, camera };
